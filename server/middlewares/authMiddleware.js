dbc394be5a6775eb342476310713a9080e39e750
Ahmed Hesham <ahmed.hesham@mtor.co>
2024-05-03 23:07:24 +0300

    fix token

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,39 +1,58 @@
-const jwt=require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const requireAuth=(req,res,next)=>{
-    const token=req.cookies.jwt;
-    if(token){
-        jwt.verify(token,process.env.JWT_SECRET_KEY,(err,decodedToken)=>{
-            if(err){
-                console.log(err.message);
-                res.redirect('/login');
-            }else{
-                console.log(decodedToken);
-                next();
-            }
-        })
-    }else{
-        res.redirect('/login')
-    }
-}
-const checkUser=(req,res,next)=>{
-    const token=req.cookies.jwt;
-    if(token){
-        jwt.verify(token,process.env.JWT_SECRET_KEY, async (err,decodedToken)=>{
-            if(err){
-                console.log(err.message);
-                res.locals.user=null;
-                next();
-            }else{
-                const user =await User.findById(decodedToken.id)
-                res.locals.user =user;
 
-                next();
-            }
-        })
-    }else{
-        res.locals.user=null;
+const requireAuth = (req, res, next) => {
+  const bearerHeader = req.headers["authorization"];
+
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+
+    req.token = bearerToken;
+
+    jwt.verify(req.token, process.env.JWT_SECRET_KEY, (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.redirect("/login");
+      } else {
+        console.log(decodedToken);
         next();
-    }
-}
-module.exports={requireAuth,checkUser};
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+};
+
+const checkUser = (req, res, next) => {
+  const bearerHeader = req.headers["authorization"];
+
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+
+    req.token = bearerToken;
+
+    jwt.verify(
+      req.token,
+      process.env.JWT_SECRET_KEY,
+      async (err, decodedToken) => {
+        if (err) {
+          console.log(err.message);
+          res.locals.user = null;
+          next();
+        } else {
+          const user = await User.findById(decodedToken.id);
+          res.locals.user = user;
+
+          next();
+        }
+      }
+    );
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { requireAuth, checkUser };
