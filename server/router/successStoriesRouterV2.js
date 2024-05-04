4f9ab749fb91cbfbb2b946fadb2a703c161d2704
ibrahemElshal <abrahimelshall@gmail.com>
2024-05-05 01:56:47 +0300

    fiex some errors

--- server/router/successStoriesRouterV2.js
+++ server/router/successStoriesRouterV2.js
@@ -79,6 +79,7 @@ router.post('/',upload.fields([{ name: 'mainPicture', maxCount: 1 }, { name: 'ad
       await newSuccessStory.save();
       res.status(201).json(newSuccessStory);
     } catch (err) {
+      res.json(err.message);
       next(err);
     }
   });
